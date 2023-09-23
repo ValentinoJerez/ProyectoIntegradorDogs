757b21daba1ea86359be7ff1bc35c28687db7bde
ValentinoJerez <valenjerez69@gmail.com>
2023-09-23 03:01:32 -0300

    Relacion entre modelos

--- api/src/db.js
+++ api/src/db.js
@@ -30,10 +30,14 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { Dog } = sequelize.models;
+const { Dog, Temperaments } = sequelize.models;
 
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
+//Relacion N a N
+
+Dog.belongsToMany(Temperaments, {through: ""});
+Temperaments.belongsToMany(Dog, {through: ""});
 
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
