b678fd68a1c74b92429ed9b7879880005e10acf1
ValentinoJerez <valenjerez69@gmail.com>
2023-09-24 23:31:58 -0300

    Creacion de Routers y Controllers

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,6 +1,11 @@
 const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
+const getDogs = require('../controllers/getDogs')
+const getDogsById = require('../controllers/getDogsById');
+const getDogsByName = require('../controllers/getDogsByName');
+const createDogs = require('../controllers/createDog');
+const getTemperaments = require('../controllers/getTemperaments')
 
 
 const router = Router();
@@ -8,5 +13,10 @@ const router = Router();
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 
+router.get("/dogs", getDogs)
+router.get("/dogs:idRaza", getDogsById);
+router.get("/dogs/name?=", getDogsByName);
+router.post("/dogs", createDogs);
+router.get("/temperaments", getTemperaments);
 
 module.exports = router;
