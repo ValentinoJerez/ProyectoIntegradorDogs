7447228f438237b8828645584f6a2bec175b953b
ValentinoJerez <valenjerez69@gmail.com>
2023-09-26 10:18:51 -0300

    Creacion de getDogs Controller y Handler

--- api/src/controllers/getDogs.js
+++ api/src/controllers/getDogs.js
@@ -1,10 +1,16 @@
 // Tipo GET
-//Importaciones
-// const {Dog} = require('../db')
+Importaciones
+const {Dog} = require('../db')
+const axios = require("axios")
 
-// const getDogs = async () => {
-//     const dogs = [];
 
-// }
+const getDogs = async () => {
+    try {
+        const response = await axios.get("https://api.thedogapi.com/v1/breeds")
+        return response.data;
+    } catch (error) {
+        throw new Error("Error")
+    }
+}
 
-// module.exports = getDogs;
+module.exports = getDogs;
--- api/src/handlers/getDogs.js
+++ api/src/handlers/getDogs.js
@@ -1,14 +1,13 @@
 //Importaciones
-// const getDogs = require("../controllers/getDogs")
+const getDogs = require("../controllers/getDogs")
 
-// const getDogs = async (req, res) => {
-//     try {
-//         const {} = req;
-//         const dogs = await getDogs()
-//         res.status(200).json(dogs)
-//     } catch (error) {
-//         res.status(500).json({error: error.message})
-//     }
-// }
+const getDogs = async (req, res) => {
+    try {
+        const dogs = await getDogs()
+        res.status(200).json(dogs)
+    } catch (error) {
+        res.status(500).json({error: error.message})
+    }
+}
 
-// module.exports = {getDogs};
+module.exports = {getDogs};
