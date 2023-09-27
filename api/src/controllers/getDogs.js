4a2aeec8ad3a1efcef08eff94771326a319e0bdc
ValentinoJerez <valenjerez69@gmail.com>
2023-09-27 17:00:22 -0300

    Cambios en getDogs

--- api/src/controllers/getDogs.js
+++ api/src/controllers/getDogs.js
@@ -5,8 +5,13 @@ const axios = require("axios")
 
 
 const get = async () => {
+        //Busco en BD
+        const dogsDB = await Dog.findAll()
+        //Busco en Api
         const response = await axios.get("https://api.thedogapi.com/v1/breeds")
-        return response.data
+        const dogsFromApi = response.data
+        //Devuelvo ambos
+        return dogsDB.concat(dogsFromApi);
 }
 
 module.exports = get;
