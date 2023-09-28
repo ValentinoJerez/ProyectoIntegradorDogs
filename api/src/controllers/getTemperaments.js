e06b95dd13c70ab1a9f1fd5af283443335966ed4
ValentinoJerez <valenjerez69@gmail.com>
2023-09-27 22:01:35 -0300

    Cambios en Temperaments

--- api/src/controllers/getTemperaments.js
+++ api/src/controllers/getTemperaments.js
@@ -3,13 +3,16 @@ const {Temperaments} = require("../db")
 const axios = require("axios")
 
 const temperaments = async () => {
+    //Pido a la Api
     const response = await axios.get("https://api.thedogapi.com/v1/breeds")
-        const guardarDB = response?.data?.map(dog => ({
-            name: dog.temperament
-        }))
-        console.log(guardarDB)
-        await Temperaments.bulkCreate(guardarDB)
-        return guardarDB
+    //Mapeo cada perro y extraigo los temperaments
+    const guardarDB = response?.data?.map(dog => ({
+        name: dog.temperament
+    }))
+    console.log(guardarDB)
+    //Guardo en la BD los temperments
+    await Temperaments.bulkCreate(guardarDB)
+    return guardarDB
 }
 
 module.exports = temperaments;
