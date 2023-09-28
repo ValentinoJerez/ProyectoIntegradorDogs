67a05cb4773585017a1019310dc63e6a3bdcd300
ValentinoJerez <valenjerez69@gmail.com>
2023-09-28 10:40:45 -0300

    Boton Enter en LandingPage

--- client/src/views/LandingPage/LandingPage.jsx
+++ client/src/views/LandingPage/LandingPage.jsx
@@ -1,10 +1,20 @@
+import { useNavigate } from "react-router-dom"
+
 import style from "../LandingPage/LandingPage.module.css"
 
 function LandingPage(){
+    const navigate = useNavigate()
+
+    //Entrar en la pagina
+    function enterPage(){
+        navigate("/Home")
+    };
+
     return(
         <div>
             <div>
             <h1>Landing Page</h1>
+            <button onClick={enterPage}>Enter</button>
             </div>
         </div>
     )
