f817ebe2963957b2d54280ae39df0ca75df42866
ValentinoJerez <valenjerez69@gmail.com>
2023-10-09 16:20:36 -0300

    Botones del paginado

--- client/src/components/Paginado/Paginado.jsx
+++ client/src/components/Paginado/Paginado.jsx
@@ -1,10 +1,10 @@
+import style from "../Paginado/Paginado.module.css"
 
-
-function Paginado({nextPage, prevPage, currentPage, cardDogs, totalPages}){
+function Paginado({nextPage, prevPage, currentPage, totalPages}){
     return(
         <div>
-            <button onClick={prevPage} disabled={currentPage === 0}>Prev</button>
-            <button onClick={nextPage} disabled={currentPage === totalPages-1}>Next</button>
+            <button className={style.prev} onClick={prevPage} disabled={currentPage === 0}>Prev</button>
+            <button className={style.next} onClick={nextPage} disabled={currentPage === totalPages-1}>Next</button>
             <h3>Pagina {currentPage + 1} de {totalPages}</h3>
         </div>
     )
